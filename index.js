17406ec93dcdf64efb4d4f8ccec6b1101984346e
woogeun <skdnrms@gamil.com>
2018-03-27 01:05:47 +0900

    express use 사용해보기

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname)));
+app.use('/se', express.static(path.join(__dirname, 'lib', 'synapeditor')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
